c1e3171a8033994c3c482f0b3785148e84596a58
JinCui <cuijin92@gmail.com>
2015-03-22 20:22:40 -0700

    delete unused code

--- app/src/main/java/com/ccframework/jc/ccframework/MainActivity.java
+++ app/src/main/java/com/ccframework/jc/ccframework/MainActivity.java
@@ -156,8 +156,6 @@ public class MainActivity extends ActionBarActivity {
             public void onClick(View v) {
 
                 Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-//                intent.setType("image/*");
-//                intent.setAction(Intent.ACTION_GET_CONTENT);
                 startActivityForResult(intent, SELECT_PICTURE);
 
 
