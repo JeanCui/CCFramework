bd81ce0259c596fd9fe802680ad41cea7b0db780
JinCui <cuijin92@gmail.com>
2015-03-30 20:47:51 -0700

    enrich Point class

--- app/src/main/java/com/ccframework/jc/ccframework/Helpers/Point.java
+++ app/src/main/java/com/ccframework/jc/ccframework/Helpers/Point.java
@@ -2,8 +2,29 @@ package com.ccframework.jc.ccframework.Helpers;
 
 /**
  * Created by Home on 3/28/2015.
+ * also use as vector
  */
 public class Point {
     public float x;
     public float y;
+
+    public Point(){}
+    public Point(float _x, float _y){
+        x = _x;
+        y = _y;
+
+    }
+    public void setPoint(float _x, float _y){
+        x = _x;
+        y = _y;
+    }
+    
+    public float getSquare(){
+        return x*x + y*y;
+    }
+    public float getValue(){
+        return (float)Math.sqrt(x*x + y*y);
+    }
+
+
 }
